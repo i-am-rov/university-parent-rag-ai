d194504e9eebb1e93ed68bfe46397fc1e2924ca2
i-am-rov <xiaourav@gmail.com>
2026-06-27 02:34:51 +0600

    Apply student scope to data queries

--- secure-student-rag-api/app/services/student_data_service.py
+++ secure-student-rag-api/app/services/student_data_service.py
@@ -1,38 +1,26 @@
 from sqlalchemy.orm import Session
 
 from app.database import Student, StudentCourse, StudentExam, StudentFee, User
-from app.services.auth_service import get_student_for_user
+from app.security.student_scope_guard import filter_query_to_student_scope, get_scoped_student_for_user
 
 
 def get_profile(db: Session, current_user: User) -> Student:
-    return get_student_for_user(db, current_user)
+    return get_scoped_student_for_user(db, current_user)
 
 
 def get_fees(db: Session, current_user: User) -> list[StudentFee]:
-    student = get_student_for_user(db, current_user)
-    return (
-        db.query(StudentFee)
-        .filter(StudentFee.student_pk == student.id)
-        .order_by(StudentFee.due_date.desc())
-        .all()
-    )
+    student = get_scoped_student_for_user(db, current_user)
+    query = filter_query_to_student_scope(db.query(StudentFee), StudentFee, student)
+    return query.order_by(StudentFee.due_date.desc()).all()
 
 
 def get_courses(db: Session, current_user: User) -> list[StudentCourse]:
-    student = get_student_for_user(db, current_user)
-    return (
-        db.query(StudentCourse)
-        .filter(StudentCourse.student_pk == student.id)
-        .order_by(StudentCourse.code)
-        .all()
-    )
+    student = get_scoped_student_for_user(db, current_user)
+    query = filter_query_to_student_scope(db.query(StudentCourse), StudentCourse, student)
+    return query.order_by(StudentCourse.code).all()
 
 
 def get_exams(db: Session, current_user: User) -> list[StudentExam]:
-    student = get_student_for_user(db, current_user)
-    return (
-        db.query(StudentExam)
-        .filter(StudentExam.student_pk == student.id)
-        .order_by(StudentExam.exam_date)
-        .all()
-    )
+    student = get_scoped_student_for_user(db, current_user)
+    query = filter_query_to_student_scope(db.query(StudentExam), StudentExam, student)
+    return query.order_by(StudentExam.exam_date).all()
